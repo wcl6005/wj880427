6a57ec5e001a7c314570cf238d1f19f00087080a
Wu Chunlong <wcl6005@126.com>
2017-11-12 21:19:03 +0800

    add 1

---
+++ Project-Account-Online/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/wuchunlong/local/wcl6005/Project-Account-Online/env/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
